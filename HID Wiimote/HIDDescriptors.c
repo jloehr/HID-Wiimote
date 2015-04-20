a56d412a7ed707388eb82f58ecfd3a96e3bda68f
Julian Löhr <jloehr_1@hotmail.com>
2015-04-20 23:10:00 +0200

    Added IR HID Report Descriptor

--- HID Wiimote/HIDDescriptors.c
+++ HID Wiimote/HIDDescriptors.c
@@ -72,6 +72,49 @@ CONST BYTE HIDReportSize = 5;
 #else
 #if defined MOUSE_IR
 
+CONST HID_REPORT_DESCRIPTOR HIDReportDescriptor[] = {
+	0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
+	0x09, 0x02,                    // USAGE (Mouse)
+	0xa1, 0x01,                    // COLLECTION (Application)
+	0x09, 0x01,                    //   USAGE (Pointer)
+	0xa1, 0x00,                    //   COLLECTION (Physical)
+	0x85, DEFAULT_REPORT_ID,	   //	  REPORT_ID (1)  
+	0x05, 0x09,                    //     USAGE_PAGE (Button)
+	0x19, 0x01,                    //     USAGE_MINIMUM (Button 1)
+	0x29, 0x03,                    //     USAGE_MAXIMUM (Button 3)
+	0x15, 0x00,                    //     LOGICAL_MINIMUM (0)
+	0x25, 0x01,                    //     LOGICAL_MAXIMUM (1)
+	0x95, 0x03,                    //     REPORT_COUNT (3)
+	0x75, 0x01,                    //     REPORT_SIZE (1)
+	0x81, 0x02,                    //     INPUT (Data,Var,Abs)
+	0x95, 0x01,                    //     REPORT_COUNT (1)
+	0x75, 0x05,                    //     REPORT_SIZE (5)
+	0x81, 0x03,                    //     INPUT (Cnst,Var,Abs)
+	0x05, 0x01,                    //     USAGE_PAGE (Generic Desktop)
+	0x09, 0x30,                    //     USAGE (X)
+	0x09, 0x31,                    //     USAGE (Y)
+	0x15, 0x81,                    //     LOGICAL_MINIMUM (-127)
+	0x25, 0x7f,                    //     LOGICAL_MAXIMUM (127)
+	0x75, 0x08,                    //     REPORT_SIZE (8)
+	0x95, 0x02,                    //     REPORT_COUNT (2)
+	0x81, 0x02,                    //     INPUT (Data,Var,Abs)
+	0xc0,                          //   END_COLLECTION
+	0xc0                           // END_COLLECTION
+};
+
+CONST BYTE HIDReportSize = 4;
+
+/*
+|--------|-------|--------|-------|--------|-------|--------|-------|
+|							  Report ID							    |
+|--------|-------|--------|-------|--------|-------|--------|-------|
+|		Button 1-3		  |xxxxxxxxxxxxxxxx Padding	xxxxxxxxxxxxxxxx|
+|--------|-------|--------|-------|--------|-------|--------|-------|
+|								X-Axis								|
+|--------|-------|--------|-------|--------|-------|--------|-------|
+|								Y-Axis								|
+*/
+
 #else
 
 /* Old
