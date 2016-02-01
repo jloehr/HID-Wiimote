8d0835b718d03e3279506ec350135c5c7f5c1fbb
Julian Löhr <jloehr_1@hotmail.com>
2016-02-01 20:30:07 +0100

    Add Control Center Application

---
+++ HID Wiimote Control Center/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HID_Wiimote_Control_Center
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
