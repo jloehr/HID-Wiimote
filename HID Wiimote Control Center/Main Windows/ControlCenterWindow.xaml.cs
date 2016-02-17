1bfb127e3f7a9c3602619a6bcf4f939deda6d422
Julian Löhr <jloehr_1@hotmail.com>
2016-02-17 17:57:48 +0100

    Move Wiimote LED Display into separate UserControl

--- HID Wiimote Control Center/Main Windows/ControlCenterWindow.xaml.cs
+++ HID Wiimote Control Center/Main Windows/ControlCenterWindow.xaml.cs
@@ -138,33 +138,4 @@ namespace HID_Wiimote_Control_Center.Main_Windows
             throw new NotImplementedException();
         }
     }
-
-    public class LEDStateToColorConverter : IValueConverter
-    {
-        public SolidColorBrush EnabledColor { get; set; }
-        public SolidColorBrush DisabledColor { get; set; }
-
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            bool[] LEDState = (bool[])value;
-            uint LEDIndex = uint.MaxValue;
-
-            if(!uint.TryParse((string)parameter, out LEDIndex))
-            {
-                return DisabledColor;
-            }
-
-            if(LEDIndex >= LEDState.Length)
-            {
-                return DisabledColor;
-            }
-
-            return (LEDState[LEDIndex]) ? EnabledColor : DisabledColor;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
-    }
 }
---
+++ HID Wiimote Control Center/UserControls/LEDDisplay.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace HID_Wiimote_Control_Center.UserControls
+{
+    /// <summary>
+    /// Interaction logic for LEDDisplay.xaml
+    /// </summary>
+    public partial class LEDDisplay : UserControl
+    {
+        public LEDDisplay()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class LEDStateToColorConverter : IValueConverter
+    {
+        public SolidColorBrush EnabledColor { get; set; }
+        public SolidColorBrush DisabledColor { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool[] LEDState = (bool[])value;
+            uint LEDIndex = uint.MaxValue;
+
+            if (!uint.TryParse((string)parameter, out LEDIndex))
+            {
+                return DisabledColor;
+            }
+
+            if (LEDIndex >= LEDState.Length)
+            {
+                return DisabledColor;
+            }
+
+            return (LEDState[LEDIndex]) ? EnabledColor : DisabledColor;
+        }       
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
