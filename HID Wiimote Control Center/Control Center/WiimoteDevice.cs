e1f3796263ff9ec6b7e25d5b5ac0c44afffe90d6
Julian Löhr <jloehr_1@hotmail.com>
2016-02-17 17:45:11 +0100

    Add WiimoteDevice Class and implemt most DataBindings

---
+++ HID Wiimote Control Center/Control Center/WiimoteDevice.cs
@@ -0,0 +1,141 @@
+﻿using System.ComponentModel;
+
+namespace HID_Wiimote_Control_Center.Control_Center
+{
+    public class WiimoteDevice : INotifyPropertyChanged
+    {
+        public enum ExtensionType { None, Nunchuck, BalanceBoard, ClassicController, WiiUProController, GuitarHero };
+        public enum ModeType { Gamepad, PassThrough, GamepadAndIRMouse, IRMouse, DPadMouse };
+
+        private ExtensionType _Extension;
+        private ModeType _Mode;
+        private byte _BatteryLevel;
+        private bool[] _LEDState;
+
+        private bool _AccelerometersEnabled;
+        private bool _XAxisEnabled;
+        private bool _YAxisEnabled;
+        private bool _MouseButtonsSwitched;
+        private bool _TriggerAndShoulderSwitched;
+        private bool _TriggerSplit;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        public WiimoteDevice(ExtensionType Extension, ModeType Mode, byte BatteryLevel, bool[] LEDState)
+        {
+            this.Extension = Extension;
+            this.Mode = Mode;
+            this.BatteryLevel = BatteryLevel;
+            this.LEDState = LEDState;
+        }
+
+        public ExtensionType Extension
+        {
+            get { return _Extension; }
+            set
+            {
+                _Extension = value;
+                OnPropertyChanged("Extension");
+            }
+        }
+
+        public ModeType Mode
+        {
+            get { return _Mode; }
+            set
+            {
+                _Mode = value;
+                OnPropertyChanged("Mode");
+            }
+        }
+
+        public byte BatteryLevel
+        {
+            get { return _BatteryLevel; }
+            set
+            {
+                _BatteryLevel = value;
+                OnPropertyChanged("BatteryLevel");
+            }
+        }
+
+        public bool[] LEDState
+        {
+            get { return _LEDState; }
+            set
+            {
+                _LEDState = value;
+                OnPropertyChanged("LEDState");
+            }
+        }
+
+        public bool AccelerometersEnabled
+        {
+            get { return _AccelerometersEnabled; }
+            set
+            {
+                _AccelerometersEnabled = value;
+                OnPropertyChanged("AccelerometersEnabled");
+            }
+        }
+
+        public bool XAxisEnabled
+        {
+            get { return _XAxisEnabled; }
+            set
+            {
+                _XAxisEnabled = value;
+                OnPropertyChanged("XAxisEnabled");
+            }
+        }
+
+        public bool YAxisEnabled
+        {
+            get { return _YAxisEnabled; }
+            set
+            {
+                _YAxisEnabled = value;
+                OnPropertyChanged("YAxisEnabled");
+            }
+        }
+
+        public bool MouseButtonsSwitched
+        {
+            get { return _MouseButtonsSwitched; }
+            set
+            {
+                _MouseButtonsSwitched = value;
+                OnPropertyChanged("MouseButtonsSwitched");
+            }
+        }
+
+        public bool TriggerAndShoulderSwitched
+        {
+            get { return _TriggerAndShoulderSwitched; }
+            set
+            {
+                _TriggerAndShoulderSwitched = value;
+                OnPropertyChanged("TriggerAndShoulderSwitched");
+            }
+        }
+
+        public bool TriggerSplit
+        {
+            get { return _TriggerSplit; }
+            set
+            {
+                _TriggerSplit = value;
+                OnPropertyChanged("TriggerSplit");
+            }
+        }
+
+        protected void OnPropertyChanged(string PropertyName)
+        {
+            PropertyChangedEventHandler Handler = PropertyChanged;
+            if (Handler != null)
+            {
+                Handler(this, new PropertyChangedEventArgs(PropertyName));
+            }
+        }
+    }
+}
--- HID Wiimote Control Center/Main Windows/ControlCenterWindow.xaml.cs
+++ HID Wiimote Control Center/Main Windows/ControlCenterWindow.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System.Windows;
+﻿using HID_Wiimote_Control_Center.Control_Center;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
 
 namespace HID_Wiimote_Control_Center.Main_Windows
 {
@@ -7,13 +13,26 @@ namespace HID_Wiimote_Control_Center.Main_Windows
     /// </summary>
     public partial class ControlCenterWindow : Window
     {
-
+        List<WiimoteDevice> ConnectedWiimoteDevices = new List<WiimoteDevice>();
 
         public ControlCenterWindow()
         {
             InitializeComponent();
+
+            ConnectedWiimoteDevices.Add(new WiimoteDevice(WiimoteDevice.ExtensionType.None, WiimoteDevice.ModeType.IRMouse, 0x3F, new bool[4] { true, true, true, true }));
+            ConnectedWiimoteDevices.Add(new WiimoteDevice(WiimoteDevice.ExtensionType.None, WiimoteDevice.ModeType.DPadMouse, 0xFF, new bool[4] { true, false, true, true }));
+            ConnectedWiimoteDevices.Add(new WiimoteDevice(WiimoteDevice.ExtensionType.Nunchuck, WiimoteDevice.ModeType.GamepadAndIRMouse, 0x1A, new bool[4] { true, true, true, true }));
+            ConnectedWiimoteDevices.Add(new WiimoteDevice(WiimoteDevice.ExtensionType.WiiUProController, WiimoteDevice.ModeType.Gamepad, 0x8C, new bool[4] { true, true, false, true }));
+            ConnectedWiimoteDevices.Add(new WiimoteDevice(WiimoteDevice.ExtensionType.BalanceBoard, WiimoteDevice.ModeType.Gamepad, 0x45, new bool[4] { true, true, false, false }));
+            ConnectedWiimoteDevices.Add(new WiimoteDevice(WiimoteDevice.ExtensionType.ClassicController, WiimoteDevice.ModeType.Gamepad, 0x9A, new bool[4] { false, true, true, false }));
+            ConnectedWiimoteDevices.Add(new WiimoteDevice(WiimoteDevice.ExtensionType.GuitarHero, WiimoteDevice.ModeType.Gamepad, 0x21, new bool[4] { false, false, true, true }));
         }
         
+        private void OnInitialized(object sender, System.EventArgs e)
+        {
+            ConnectedDevicesListBox.ItemsSource = ConnectedWiimoteDevices;
+        }
+
         private void OnInstallerClick(object sender, RoutedEventArgs e)
         {
             App.ChangeMainWindow(new InstallerWindow(), this);
@@ -25,6 +44,127 @@ namespace HID_Wiimote_Control_Center.Main_Windows
             AboutDialog.Owner = this;
 
             AboutDialog.ShowDialog();
+        }
+
+        private static Dictionary<WiimoteDevice.ModeType, string> WiimoteDeviceModeTypeDescriptionsDictionary = new Dictionary<WiimoteDevice.ModeType, string>()
+        {
+            { WiimoteDevice.ModeType.Gamepad, HID_Wiimote_Control_Center.Properties.ControlCenter.ModeString_Gamepad },
+            { WiimoteDevice.ModeType.PassThrough, HID_Wiimote_Control_Center.Properties.ControlCenter.ModeString_PassThrough },
+            { WiimoteDevice.ModeType.GamepadAndIRMouse, HID_Wiimote_Control_Center.Properties.ControlCenter.ModeString_GamepadAndIRMouse },
+            { WiimoteDevice.ModeType.IRMouse, HID_Wiimote_Control_Center.Properties.ControlCenter.ModeString_IRMouse },
+            { WiimoteDevice.ModeType.DPadMouse, HID_Wiimote_Control_Center.Properties.ControlCenter.ModeString_DPadMouse }
+        };
+
+        public Dictionary<WiimoteDevice.ModeType, string> WiimoteDeviceModeTypeDescriptions
+        {
+            get
+            {
+                return WiimoteDeviceModeTypeDescriptionsDictionary;
+            }
+        }
+
+    }
+
+    public class ExtensionToStringConverter : IValueConverter
+    {
+        public string WiimoteString { get; set; }
+        public string NunchuckString { get; set; }
+        public string ClassicControllerString { get; set; }
+        public string WiiUProControllerString { get; set; }
+        public string BalanceBoardString { get; set; }
+        public string GuitarHeroString { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            WiimoteDevice.ExtensionType Extension = (WiimoteDevice.ExtensionType)value;
+
+            switch (Extension)
+            {
+                case WiimoteDevice.ExtensionType.None:
+                    return WiimoteString;
+                case WiimoteDevice.ExtensionType.Nunchuck:
+                    return NunchuckString;
+                case WiimoteDevice.ExtensionType.BalanceBoard:
+                    return BalanceBoardString;
+                case WiimoteDevice.ExtensionType.ClassicController:
+                    return ClassicControllerString;
+                case WiimoteDevice.ExtensionType.WiiUProController:
+                    return WiiUProControllerString;
+                case WiimoteDevice.ExtensionType.GuitarHero:
+                    return GuitarHeroString;
+                default:
+                    return "Error";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ModeToStringConverter : IValueConverter
+    {
+        public string GamepadString { get; set; }
+        public string GamepadAndIRMouseString { get; set; }
+        public string IRMouseString { get; set; }
+        public string DPadMouseString { get; set; }
+        public string PassThroughString { get; set; }
+
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            WiimoteDevice.ModeType Mode = (WiimoteDevice.ModeType)value;
+
+            switch (Mode)
+            {
+                case WiimoteDevice.ModeType.Gamepad:
+                    return GamepadString;
+                case WiimoteDevice.ModeType.PassThrough:
+                    return PassThroughString;
+                case WiimoteDevice.ModeType.GamepadAndIRMouse:
+                    return GamepadAndIRMouseString;
+                case WiimoteDevice.ModeType.IRMouse:
+                    return IRMouseString;
+                case WiimoteDevice.ModeType.DPadMouse:
+                    return DPadMouseString;
+                default:
+                    return "Error";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class LEDStateToColorConverter : IValueConverter
+    {
+        public SolidColorBrush EnabledColor { get; set; }
+        public SolidColorBrush DisabledColor { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool[] LEDState = (bool[])value;
+            uint LEDIndex = uint.MaxValue;
+
+            if(!uint.TryParse((string)parameter, out LEDIndex))
+            {
+                return DisabledColor;
+            }
+
+            if(LEDIndex >= LEDState.Length)
+            {
+                return DisabledColor;
+            }
+
+            return (LEDState[LEDIndex]) ? EnabledColor : DisabledColor;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
         }
     }
 }
